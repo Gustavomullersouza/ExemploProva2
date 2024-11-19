5fbf1f3f982e0a7ac319f01441e928e7fd34cceb
marcolapcx <168039947+marcolapcx@users.noreply.github.com>
2024-11-18 22:24:56 -0300

    ExemploProva

--- src/ExemploProva.java
+++ src/ExemploProva.java
@@ -1,2 +1,41 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class ExemploProva {
+    private JTextField txtValor;
+    private JPanel panelMain;
+    private JButton btnExibir;
+    private JButton BtnSomar;
+
+    Integer valor = 0;
+
+    String operacao = "";
+
+    public String somar(Integer num1, Integer num2) {
+        return String.valueOf(num1 + num2+ 2.5);
+    }
+
+    public ExemploProva() {
+        btnExibir.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Integer valorDois = Integer.valueOf(txtValor.getText());
+                  Integer soma = 0;
+                if (operacao.equals("+")){
+                    soma = valor + valorDois;
+                }
+
+                txtValor.setText(String.valueOf(soma));
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame tela = new JFrame();
+        tela.setTitle("Minha TV");
+        tela.setSize(500, 500);
+        tela.setContentPane(new ExemploProva().panelMain);
+        tela.setVisible(true);
+    }
 }
